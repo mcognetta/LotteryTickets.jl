39291d6c8e03a047632c24ae81b399c0bc81773f
Marco Cognetta <cognetta.marco@gmail.com>
2023-07-05 20:22:32 +0900

    please

--- src/new.jl
+++ src/new.jl
@@ -14,6 +14,16 @@ function FakeDense(d::Dense)
     FakeDense(d, orig, mask)
 end
 
+Zygote.@adjoint function (f::FakeDense)(x)
+    f.d.weight .*= f.mask
+    # println("YO ", x)
+    # println(f)
+    out, pb = Zygote._pullback(f.d, x)
+    # println("OUT ", out)
+    # (f(x), ŷ -> (println("PB ", pb(ŷ)[1][1]); (pb(ŷ)[1][1] .* f.mask, pb(ŷ .* f.mask)[2])))
+    f(x), ŷ -> (pb(ŷ)[1][1] .* f.mask, pb(ŷ .* f.mask)[2])
+end
+
 struct FakeConv2
     c
     orig
@@ -31,6 +41,18 @@ end
 Flux.@functor FakeConv2
 
 Flux.trainable(c::FakeConv2) = Flux.trainable(c.c)
+
+
+julia> Zygote.@adjoint function (f::FakeConv2)(x)
+    f.c.weight .*= f.mask
+    # println("YO ", x)
+    # println(f)
+    out, pb = Zygote._pullback(f.c, x)
+    # println("OUT ", out)
+    # println("f(x)", f(x))
+    # (f(x), ŷ -> (println(ŷ); println(size(ŷ)); println("YHAT ", size(pb(ŷ)[1][2])); (pb(ŷ)[1][2] .* f.mask, pb(ŷ)[2])))
+    f(x), ŷ -> (pb(ŷ)[1][2] .* f.mask, pb(ŷ)[2])
+end
 
 """
 
